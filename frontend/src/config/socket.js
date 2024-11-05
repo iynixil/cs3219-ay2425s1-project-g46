be198964bd1767bb6c715f490b4237fe3891b94f
tsulim <limxiujia18@gmail.com>
2024-11-06 04:06:57 +0800

    Update `.env.sample`

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
+import { COLLABORATION_SOCKET_URL, MATCHING_SOCKET_URL } from "./url";
 
-const matchingSocket = io(process.env.MATCHING_SOCKET_URL || 'http://localhost:5002');
-const collaborationSocket = io(process.env.COLLABORATION_SOCKET_URL || 'http://localhost:5003');
+const matchingSocket = io(MATCHING_SOCKET_URL || 'http://localhost:5002');
+const collaborationSocket = io(COLLABORATION_SOCKET_URL || 'http://localhost:5003');
 
 
 export { matchingSocket, collaborationSocket };
--- frontend/src/config/url.js
+++ frontend/src/config/url.js
@@ -1,1 +1,4 @@
 export const API_GATEWAY_URL = process.env.REACT_APP_API_GATEWAY_URL || "http://localhost:8000/api";
+
+export const MATCHING_SOCKET_URL = process.env.REACT_APP_MATCHING_SOCKET_URL || "http://localhost:5002";
+export const COLLABORATION_SOCKET_URL = process.env.REACT_APP_COLLABORATION_SOCKET_URL || "http://localhost:5003";
