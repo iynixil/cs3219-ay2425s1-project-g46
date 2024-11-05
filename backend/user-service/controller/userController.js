47b7faca2ee1f41ca7f0f2accaf4e4ddc05ae9b2
iynixil <lixinyilxy02@gmail.com>
2024-11-06 00:24:24 +0800

    Add functionality to prevent user from logging in on another browser or device

--- backend/user-service/controller/userController.js
+++ backend/user-service/controller/userController.js
@@ -11,17 +11,17 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 // Hash password function
 async function hashPass(password) {
-return await bcrypt.hash(password, 10);
+  return await bcrypt.hash(password, 10);
 }
 
 // sign up API
 const signup = async (req, res) => {
-try {
+  try {
     const usersRef = userCollection.doc(req.body.email);
     const getUser = await usersRef.get();
-    
+
     if (getUser.exists) {
-        return res.status(400).send({ message: "Email already exists. Please use another email." });
+      return res.status(400).send({ message: "Email already exists. Please use another email." });
     }
 
     const username = req.body.name;
@@ -30,67 +30,87 @@ try {
     const encryptedPassword = await hashPass(password);
 
     const userJson = {
-        username: username,
-        email: email,
-        password: encryptedPassword
+      username: username,
+      email: email,
+      password: encryptedPassword,
+      isLoggedIn: false
     };
 
     const response = await userCollection.doc(email).set(userJson);
     res.send({ message: "User created successfully", response });
     console.log({ message: "User created successfully", response });
-    
-} catch (error) {
+
+  } catch (error) {
     res.status(500).send({ error: error.message });
     console.log({ error: error.message });
-}
+  }
 };
 
 // login API
 const login = async (req, res) => {
-const usersRef = userCollection.doc(req.body.email);
-const getUser = await usersRef.get();
-if (getUser.exists) {
+  const usersRef = userCollection.doc(req.body.email);
+  const getUser = await usersRef.get();
+  if (getUser.exists) {
+    // check if user currently has another login session
+    const isLoggedIn = getUser.get("isLoggedIn");
+
+    if (isLoggedIn) {
+      return res.status(403).send({
+        message: "This user is already logged in." +
+          "\nPlease terminate your current session that might be in another browser or device."
+      })
+    }
+
     // get the hashed password in database to compare
     const userPassword = getUser.get("password");
 
     // check if password entered is same as password in database
     bcrypt.compare(req.body.password, userPassword, (error, compareResult) => {
-    // if some error occurs
-    if (error) {
+      // if some error occurs
+      if (error) {
         console.log(error);
-    }
-    // if passwords do not match
-    if (!compareResult) {
+      }
+      // if passwords do not match
+      if (!compareResult) {
         console.log("Password is incorrect.");
         return res.status(500).send({ message: "Password is incorrect." });
-    }
+      }
 
-    // passwords match, successful login
-    // generate a token for the login session,
-    // return the token, email and username of logged-in user
-    const payload = { email: req.body.email };
-    jwt.sign(payload, JWT_SECRET, { algorithm: "HS256" }, (error, token) => {
+      // passwords match, successful login
+      // generate a token for the login session,
+      // return the token, email and username of logged-in user
+      const payload = { email: req.body.email };
+      jwt.sign(payload, JWT_SECRET, { algorithm: "HS256" }, (error, token) => {
         if (error) {
-        console.log(error);
-        return res.status(401).send();
+          console.log(error);
+          return res.status(401).send();
         }
-        res.status(200).send({
-        token: token,
-        email: req.body.email,
-        username: getUser.get("username")
+        // set the login status of this user to true
+        usersRef.update({ "isLoggedIn": true });
+        // log message of the user logging in
+        console.log("User " + req.body.email + " is logged in.");
+        return res.status(200).send({
+          token: token,
+          email: req.body.email,
+          username: getUser.get("username")
         });
+      })
     })
-    })
-} else {
+  } else {
     return res.status(404).send({ message: "No user associated with this email. Please sign up for an account." });
-}
+  }
 };
 
 // logout API
 const logout = async (req, res) => {
-    console.log("User is logging out.");
-    res.clearCookie('session-id');
-    return res.status(200).send({ message: "Logout successful." });
+  // set the login status of this user to false
+  const usersRef = userCollection.doc(req.body.email);
+  usersRef.update({ "isLoggedIn": false });
+
+  // log message of the user logging out
+  console.log("User " + req.body.email + " is logging out.");
+  res.clearCookie('session-id');
+  return res.status(200).send({ message: "Logout successful." });
 };
 
 // Export user functions
