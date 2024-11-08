36140fbec9430da823e16936a0ecd038432a0197
AndrewOng2066 <e0959899@u.nus.edu>
2024-11-08 21:03:12 +0800

    Remove all the feedback-service components

--- backend/feedback-service/controller/feedbackController.js
+++
@@ -1,6 +0,0 @@
-// // Author(s): Andrew
-// const db = require("../config/firebase");
-// const userReviewCollection = db.collection("userReviews");
-// const transporter = require("../config/nodemailer"); 
-
-
