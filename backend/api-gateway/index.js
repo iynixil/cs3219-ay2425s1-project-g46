31197ed642472559ce8e24d3349ef1aa2eb9c4c7
iynixil <lixinyilxy02@gmail.com>
2024-11-11 16:53:27 +0800

    Change rate limiter settings

--- backend/api-gateway/index.js
+++ backend/api-gateway/index.js
@@ -33,6 +33,9 @@ const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again later.',
+  validate: {
+    xForwardedForHeader: false
+  },
 
   // Custom handler function for rate limit errors
   handler: (req, res) => {
